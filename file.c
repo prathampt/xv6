4c1897c0bdc3251ab6ef86dcb0637bca26fdba9c
Pratham Tandale <prathamt12345@gmail.com>
2025-03-27 20:41:15 +0530

    added code for slab allocator in file.c

--- file.c
+++ file.c
@@ -10,16 +10,46 @@
 #include "sleeplock.h"
 #include "file.h"
 
+#define MAXFILE_PERPAGE 160 // Calculated for 4KB page
+
 struct devsw devsw[NDEV];
+
+struct fileslab {
+    int freecount;
+    uint bitmap[5]; // to represent 5*4*8 = 160 file structures
+    struct fileslab *next;
+    struct fileslab *prev;
+    int padding[64 - 8]; // (4k - 160 * 24) = 256 and 256 / 4 = 64
+    struct file files[MAXFILE_PERPAGE];
+}; // Size of the struct is 4KB :)
+
 struct {
   struct spinlock lock;
-  struct file file[NFILE];
+  struct fileslab *head;
 } ftable;
+
+struct fileslab *get_file_slab(){
+    struct fileslab *fileslab = (struct fileslab *)kalloc();
+    fileslab->freecount = MAXFILE_PERPAGE;
+    for (int i = 0; i < 5; i++) {
+        fileslab->bitmap[i] = 0;
+    }
+    fileslab->next = 0;
+    fileslab->prev = 0;
+
+    return fileslab;
+}
 
 void
 fileinit(void)
 {
   initlock(&ftable.lock, "ftable");
+  struct fileslab *new_slab = get_file_slab();
+  new_slab->next = new_slab;
+  new_slab->prev = new_slab;
+  acquire(&ftable.lock);
+  ftable.head = new_slab;
+  release(&ftable.lock);
 }
 
 // Allocate a file structure.
@@ -27,16 +57,33 @@ struct file*
 filealloc(void)
 {
   struct file *f;
-
+  struct fileslab *slab;
   acquire(&ftable.lock);
-  for(f = ftable.file; f < ftable.file + NFILE; f++){
-    if(f->ref == 0){
-      f->ref = 1;
-      release(&ftable.lock);
-      return f;
-    }
+  if(!ftable.head)
+      panic("filealloc: not initialized ");
+  if(ftable.head->freecount == 0) {
+      struct fileslab *new_slab = get_file_slab();
+      new_slab->prev = ftable.head;
+      new_slab->next = ftable.head->next;
+      ftable.head->next = new_slab;
+      new_slab->next->prev = new_slab;
+      ftable.head = new_slab;
   }
-  release(&ftable.lock);
+  slab = ftable.head;
+  for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 32; j++) {
+          uint mask = 1U << j;
+          if (!(slab->bitmap[i] & mask)) {
+              slab->bitmap[i] |= mask;
+              slab->freecount--;
+              f = &slab->files[i * 32 + j];
+              f->ref = 1;
+              release(&ftable.lock);
+              return f;
+          }
+      }
+  }
+  panic("filealloc: ");
   return 0;
 }
 
@@ -57,6 +104,8 @@ void
 fileclose(struct file *f)
 {
   struct file ff;
+  struct fileslab *slabaddr;
+  int free = 0;
 
   acquire(&ftable.lock);
   if(f->ref < 1)
@@ -67,8 +116,25 @@ fileclose(struct file *f)
   }
   ff = *f;
   f->ref = 0;
-  f->type = FD_NONE;
+  // Violating some laws under the name of optimization
+  // Find addr of parent struct
+  // As each page is aligned at 4KB boundary
+  slabaddr = (struct fileslab *)((int)f & ~0xFFF);
+  int index = f - slabaddr->files;
+
+  if (index < 0 || index >= MAXFILE_PERPAGE)
+      panic("invalid file pointer");
+  slabaddr->bitmap[index / 32] &= ~(1U << (index % 32));
+  if (slabaddr->freecount == MAXFILE_PERPAGE && slabaddr->next != slabaddr) {
+      free = 1;
+      ftable.head = slabaddr->prev;
+      slabaddr->prev->next = slabaddr->next;
+      slabaddr->next->prev = slabaddr->prev;
+  }
   release(&ftable.lock);
+
+  if(free)
+      kfree((char *)slabaddr);
 
   if(ff.type == FD_PIPE)
     pipeclose(ff.pipe, ff.writable);
