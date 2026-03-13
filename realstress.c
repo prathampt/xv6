1c020e7199fcdd4e735dc050fbdcd32a77e80e8b
Arjun-Deodhar <arjundeodhar@gmail.com>
2026-03-13 13:35:58 +0530

    feat: module 4 added

--- realstress.c
+++ realstress.c
@@ -463,6 +463,313 @@ concreate(int seed)
   return 0;
 }
 
+//
+// Module 4
+//
+
+// does exec return an error if the arguments
+// are larger than a page? or does it write
+// below the stack and wreck the instructions/data?
+int
+bigargtest(int seed)
+{
+  int pid, fd;
+  char okfile[16];
+
+  // This file is opened just to allow the parent to check
+  // whether exec() failed in the child
+  strcpy(okfile, "bigarg-ok");
+  okfile[strlen(okfile)] = 'a' + seed % 26;
+  okfile[strlen(okfile)] = 0;
+
+  unlink(okfile);
+  pid = fork();
+  if(pid == 0){
+    static char *args[MAXARG];
+    int i;
+    for(i = 0; i < MAXARG-1; i++)
+      args[i] = "bigargs test: failed\n                                                                                                                                                                                                       ";
+    args[MAXARG-1] = 0;
+    exec("echo", args);
+    fd = open(okfile, O_CREATE);
+    close(fd);
+    exit();
+  } else if(pid < 0){
+    printf(1, "bigargtest: fork failed\n");
+    return 1;
+  }
+  wait();
+  fd = open(okfile, 0);
+  if(fd < 0){
+    printf(1, "bigargtest: open failed!\n");
+    return 1;
+  }
+  close(fd);
+  unlink(okfile);
+  return 0;
+}
+
+// test writes that are larger than the log.
+int
+bigwrite(int seed)
+{
+  printf(1, "%d\n", seed);
+  int fd, sz;
+  // added more blocks and another child for increased load
+  int nblocks = 32;
+  int pid = fork();
+
+  unlink("bigwrite");
+  for(sz = 499; sz < nblocks*512; sz += 471){
+    fd = open("bigwrite", O_CREATE | O_RDWR);
+    if(fd < 0){
+      printf(1, "bigwrite: cannot create bigwrite\n");
+      return 1;
+    }
+    int i;
+    for(i = 0; i < 2; i++){
+      int cc = write(fd, buf, sz);
+      if(cc != sz){
+        printf(1, "bigwrite: write(%d) ret %d\n", sz, cc);
+        return 1;
+      }
+    }
+    close(fd);
+  }
+  if(pid == 0)
+    exit();
+  else
+    wait();
+
+  return 0;
+}
+
+// does unintialized data start out zero?
+char uninit[8749];
+int
+bsstest(int seed)
+{
+  printf(1, "%d\n", seed);
+  int i;
+  for(i = 0; i < sizeof(uninit); i++){
+    if(uninit[i] != '\0'){
+      printf(1, "bsstest: failed\n");
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int
+sbrktest(int seed)
+{
+  printf(1, "%d\n", seed);
+  int fds[2], pid, pids[10], ppid;
+  char *a, *b, *c, *lastaddr, *oldbrk, *p, scratch;
+  uint amt;
+
+  oldbrk = sbrk(0);
+
+  // can one sbrk() less than a page?
+  a = sbrk(0);
+  int i;
+  for(i = 0; i < 5000; i++){
+    b = sbrk(1);
+    if(b != a){
+      printf(1, "sbrktest: failed %d %x %x\n", i, a, b);
+      return 1;
+    }
+    *b = 1;
+    a = b + 1;
+  }
+  pid = fork();
+  if(pid < 0){
+    printf(1, "sbrktest: fork failed\n");
+    return 1;
+  }
+  c = sbrk(1);
+  c = sbrk(1);
+  if(c != a + 1){
+    printf(1, "sbrktest: failed post-fork\n");
+    return 1;
+  }
+  if(pid == 0)
+    exit();
+  wait();
+
+  // can one grow address space to something big?
+#define BIG ((PHYSTOP/4) - 6)
+  a = sbrk(0);
+  amt = (BIG) - (uint)a;
+  p = sbrk(amt);
+  if (p != a) {
+    printf(1, "sbrktest: failed to grow big address space; enough phys mem?\n");
+    return 1;
+  }
+  lastaddr = (char*) (BIG-1);
+  *lastaddr = 99;
+
+  // can one de-allocate?
+  a = sbrk(0);
+  c = sbrk(-4096);
+  if(c == (char*)0xffffffff){
+    printf(1, "sbrktest: could not deallocate\n");
+    return 1;
+  }
+  c = sbrk(0);
+  if(c != a - 4096){
+    printf(1, "sbrktest: deallocation produced wrong address, a %x c %x\n", a, c);
+    return 1;
+  }
+
+  // can one re-allocate that page?
+  a = sbrk(0);
+  c = sbrk(4096);
+  if(c != a || sbrk(0) != a + 4096){
+    printf(1, "sbrktest: re-allocation failed, a %x c %x\n", a, c);
+    return 1;
+  }
+  if(*lastaddr == 99){
+    // should be zero
+    printf(1, "sbrktest: de-allocation didn't really deallocate\n");
+    return 1;
+  }
+
+  a = sbrk(0);
+  c = sbrk(-(sbrk(0) - oldbrk));
+  if(c != a){
+    printf(1, "sbrktest: downsize failed, a %x c %x\n", a, c);
+    return 1;
+  }
+
+  // can we read the kernel's memory?
+  for(a = (char*)(KERNBASE); a < (char*) (KERNBASE+2000000); a += 50000){
+    ppid = getpid();
+    pid = fork();
+    if(pid < 0){
+      printf(1, "sbrktest: fork failed\n");
+      return 1;
+    }
+    if(pid == 0){
+      printf(1, "sbrktest: oops could read %x = %x\n", a, *a);
+      kill(ppid);
+      return 1;
+    }
+    wait();
+  }
+
+  // if we run the system out of memory, does it clean up the last
+  // failed allocation?
+  if(pipe(fds) != 0){
+    printf(1, "sbrktest: pipe() failed\n");
+    return 1;
+  }
+  for(i = 0; i < sizeof(pids)/sizeof(pids[0]); i++){
+    if((pids[i] = fork()) == 0){
+      // allocate a lot of memory
+      sbrk(BIG - (uint)sbrk(0));
+      write(fds[1], "x", 1);
+      // sit around until killed
+      for(;;) sleep(1000);
+    }
+    if(pids[i] != -1)
+      read(fds[0], &scratch, 1);
+  }
+  // if those failed allocations freed up the pages they did allocate,
+  // we'll be able to allocate here
+  c = sbrk(4096);
+  for(i = 0; i < sizeof(pids)/sizeof(pids[0]); i++){
+    if(pids[i] == -1)
+      continue;
+    kill(pids[i]);
+    wait();
+  }
+  if(c == (char*)0xffffffff){
+    printf(1, "sbrktest: failed sbrk leaked memory\n");
+    return 1;
+  }
+
+  if(sbrk(0) > oldbrk)
+    sbrk(-(sbrk(0) - oldbrk));
+
+  return 0;
+}
+
+void
+validateint(int *p)
+{
+  int res;
+  asm("mov %%esp, %%ebx\n\t"
+      "mov %3, %%esp\n\t"
+      "int %2\n\t"
+      "mov %%ebx, %%esp" :
+      "=a" (res) :
+      "a" (SYS_sleep), "n" (T_SYSCALL), "c" (p) :
+      "ebx");
+}
+
+int
+validatetest(int seed)
+{
+  printf(1, "%d\n", seed);
+  int hi, pid;
+  uint p;
+
+  hi = 8192*1024; // increased for more load
+
+  for(p = 0; p <= (uint)hi; p += 734){
+    if((pid = fork()) == 0){
+      // try to crash the kernel by passing in a badly placed integer
+      validateint((int*)p);
+      exit();
+    }
+    sleep(0);
+    sleep(0);
+    kill(pid);
+    wait();
+
+    // try to crash the kernel by passing in a bad string pointer
+    if(link("nosuchfile", (char*)p) != -1){
+      printf(1, "link should not succeed\n");
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int (*module_4[])(int) = {
+  [0] bigargtest,
+  [1] bigwrite,
+  [2] bsstest,
+  [3] sbrktest,
+  [4] validatetest,
+};
+
+void
+module4(void)
+{
+  int uptime0 = uptime();
+  printf(1, "module 4 started\n");
+  int ret, i;
+  for(i = 0; i < NELEM(module_4)*NTHREADS; i++) {
+    if(fork() == 0) {
+      ret = module_4[i/NTHREADS](i%NTHREADS);
+      if(ret) {
+        dopanic("TEST FAILED\n");
+      }
+      exit();
+    }
+  }
+  // wait for all children
+  for(i = 0; i < NELEM(module_4)*NTHREADS; i++) {
+    wait();
+  }
+  unlink("bigwrite");  // bigwrite
+
+  printf(1, "module 4 passed\n");
+  printf(1, "module 4 time: %d\n", uptime() - uptime0);
+}
+
 int (*module_1[])(int) = {
   [0] argptest,
   [1] createdelete,
@@ -578,6 +885,7 @@ main(int argc, char *argv[])
   module1();
   module2();
   module3();
+  module4();
   printf(1, "Total time: %d\n", uptime() - uptime0);
 
   exec("shutdown", shutdown_argv);
